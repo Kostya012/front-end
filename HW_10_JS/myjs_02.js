febbce2d11b7a037a5fc54b479cb989b0a04ce10
Kostya012 <61709439+Kostya012@users.noreply.github.com>
2020-03-29 23:54:39 +0300

    fixed

--- HW_10_JS/myjs_02.js
+++ HW_10_JS/myjs_02.js
@@ -72,15 +72,16 @@ let way2 = rand1 ** 2 - rand2 ** 2//-3
 console.log(way1, way2)
 
 //Task with * 1
-let yourNu1 = prompt('Введите число 1 из 3:', 'Name')
-let yourNu2 = prompt('Введите число 2 из 3:', 'Two Name')
+let yourNu1 = prompt('Введите число 1 из 3:', 0)
+let yourNu2 = prompt('Введите число 2 из 3:', 0)
 let yourNu3 = prompt('Введите число 3 из 3:', 0)
 
 let res1 = Number(yourNu1) + 10
 let res2 = Number(yourNu2) + 10
 let res3 = Number(yourNu3) + 10
+let sum = res1 + res2 + res3
 //yourAge -> translate from String to Number
-alert(`Ваше первое число +10: ${res1}, Ваше второе число +10: ${res2}, Ваше третье число +10: ${res3}`)
+alert(`Ваше первое число +10: ${res1}, Ваше второе число +10: ${res2}, Ваше третье число +10: ${res3} их сумма = ${sum}`)
 
 //Task with * 2
 let yourNumb1 = prompt('Введите одну сторону треугольника:', 0)
@@ -95,4 +96,5 @@ console.log("Треугольник:", re1 === re2 === re3)
 let numA = 3
 let numB = 2
 let way3 = (numA + numB) ** 3
-console.log(way3)
+let way4 = numA ** 3 + 3 * numA ** 2 * numB + 3 * numA * numB ** 2 + numB ** 3
+console.log(way3, way4)
